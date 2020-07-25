16458f8a24fd0778077a05393af98822cbec6952
Marcel Braun <marcel@strg.cc>
2020-07-25 21:42:18 +0200

    Use custom file exists checking

--- source/fileExists.ts
+++ source/fileExists.ts
@@ -1,12 +1,9 @@
 export default async (path: string): Promise<boolean> => {
     try {
         await Deno.stat(path);
-        return true;
     } catch (error) {
-        if (error && !error.kind === Deno.ErrorKind.NotFound) {
-            throw error;
-        }
+        return false;
     }
 
-    return false;
+    return true;
 };
