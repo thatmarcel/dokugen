141b31df95fd818772f4edcbc37e585683ec2afc
Marcel Braun <marcel@strg.cc>
2020-07-25 22:03:43 +0200

    Fetch HTML from server

--- source/wrap.ts
+++ source/wrap.ts
@@ -1,5 +1,7 @@
+import getHTML from "./getHTML.ts";
+
 export default async (pageHTML: string, title: string): Promise<string> => {
-    const template = await Deno.readTextFile("./source/templates/general.html");
+    const template = await getHTML("templates/general.html");
 
     return template.replace("{page.title}", title).replace("{page.content}", pageHTML);
 }
